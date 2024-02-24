295a9819bb88386d43a146ea0017bbbf33064a84
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2024-02-24 19:14:24 +0300

    Update DisplayService.java

--- src/main/java/service/DisplayService.java
+++ src/main/java/service/DisplayService.java
@@ -2,7 +2,23 @@ package service;
 
 import java.util.List;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import view.Dashboard;
 
-public interface DisplayService<T> {
-    void displayData(List<T> t, JTable table);
+public final class DisplayService<T> {
+    public void displayData(List<T> data, JTable table, TableConfig<T> tableConfig) {
+        DefaultTableModel model = Dashboard.getModel();
+        model.setRowCount(0);
+        model.setColumnCount(0);
+        
+        String[] columns = tableConfig.getColumnNames();
+        for (String s: columns)
+            model.addColumn(s);
+        
+        for (T t: data)
+            model.addRow(tableConfig.getRowData(t));
+        
+        Dashboard.setModel(model);
+        table.setModel(model);
+    }
 }
